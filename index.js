32edf29d71571ae42a2a969859698d2c86bd96cf
yudang12 <jjg121218@gamil.com>
2025-04-11 17:41:29 +0900

    second

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 let express = require("express");
-// axios 모듈을 사용하기 위한 선언
+// const { console } = require("inspector");
 let axios = require("axios");
-let app = express(); //앱서버함수저장
-let port = process.env.PORT || 80;  
 
-app.use(express.static("public_html")); 
+let app = express();
+let port = process.env.PORT || 80;
+
+app.use(express.static("public_html"));
 app.listen(port, function(){
-    console.log("HTML 서버 시작됨");
+	console.log("html 서버 시작됨")
 });
 
-//https://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?serviceKey=bHQPhKEgeXQ6YbbdoSpEpUNZEtSTE4umoIOw4Oj9V6MLRf8%2BOj9iSrv8xSR8X6mezmZaHJDGACDkp5mcmTTv8w%3D%3D&Q0=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&Q1=%EA%B0%95%EB%82%A8%EA%B5%AC&QT=1&QN=%EC%82%BC%EC%84%B1%EC%95%BD%EA%B5%AD&ORD=NAME&pageNo=1&numOfRows=10    //약국 소스 미리보기?
 
 // app.use()로 public_html 폴더 전체를 express 모듈 웹서버가 구동되게 했는데, 단 한 개의 페이지만 열어보자
 // 두 번째 파라미터에는 /pharmach_list로 접속한 사용자에게 (req, res)를 사용하여 요청(받고)과 응답(보냄)
@@ -41,8 +41,7 @@ app.get("/pharmach_list", (req, res) => {
             });
         }
         catch(e) {
-            console.error(e);
-            res.status(500).json({ error: "데이터를 불러오는 중 오류가 발생했습니다." });
+            console.log(e);
         }
         return response;
     }
@@ -52,4 +51,10 @@ app.get("/pharmach_list", (req, res) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(response.data.response.body);
     });        
+});
+
+
+// index02.html에 접근 가능하도록 라우팅 추가
+app.get("/index02", (req, res) => {
+    res.sendFile(__dirname + "/public_html/index02.html");
 });
